827cb753bbbbb9b584b9884f1e1b7eaee599e56b
owias azizi <owaisazizi360@gmail.com>
2025-01-12 13:06:53 +0430

    test get and post Request

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 
+//get is http method for request and the data is the respose from server
 app.get('/',(req,res)=>{
-   res.status(200).send('Hello from Server!')
+//    res.status(200).send('Hello from Server!')
+   res.status(200).json({message:'Hello from server(get)',app:'natrouse'})
 })
+
+app.post('/',(req,res)=>{
+       res.status(200).send('you can post here(post)')
+    })
 
 const port = 3000
 app.listen(port,()=>{
