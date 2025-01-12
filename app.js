88f262ca28006767fcc0f2c0a88b54b7481263ca
owias azizi <owaisazizi360@gmail.com>
2025-01-12 16:04:41 +0430

    get some data f

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
 
-//get is http method for request and the data is the respose from server
-app.get('/',(req,res)=>{
-//    res.status(200).send('Hello from Server!')
-   res.status(200).json({message:'Hello from server(get)',app:'natrouse'})
-})
+// //get is http method for request and the data is the respose from server
+// app.get('/',(req,res)=>{
+// //    res.status(200).send('Hello from Server!')
+//    res.status(200).json({message:'Hello from server(get)',app:'natrouse'})
+// })
 
-app.post('/',(req,res)=>{
-       res.status(200).send('you can post here(post)')
-    })
+// app.post('/',(req,res)=>{
+//        res.status(200).send('you can post here(post)')
+//     })
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours',(req,res)=>{
+   res.status(200).json({
+       status:'success',
+       result:tours.lenght,
+    data:{
+        tours
+    }
+   })
+})
 
 const port = 3000
 app.listen(port,()=>{
