7be419460ff66737be3c1ece72cb5570074f097e
owias azizi <owaisazizi360@gmail.com>
2025-01-14 12:38:54 +0430

    create sever file

--- app.js
+++ app.js
@@ -21,8 +21,4 @@ app.use((req,res,next)=>{
 app.use('/api/v1/tours',tourRouter)
 app.use('/api/v1/users',userRouter)
 
-//start server
-const port = 3000
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+module.exports = app
