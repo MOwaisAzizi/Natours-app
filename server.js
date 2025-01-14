be24b8690f3cfcc1be3b14e3f500dd0ab6fb97fd
owias azizi <owaisazizi360@gmail.com>
2025-01-14 17:08:54 +0430

    create confic.env

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const app = require('./app')
 
+//express environment
+console.log(app.get('env'));
+//node uses many envirnment
+console.log(process.env);
+
 const port = 3000
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
