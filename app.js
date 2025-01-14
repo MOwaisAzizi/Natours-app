914bbfcca12c08ff41ba3f551fe14b0b264e6b53
owias azizi <owaisazizi360@gmail.com>
2025-01-14 16:39:35 +0430

    accessing to static files

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const tourRouter = require('./routes/tourRouter')
 
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
     // console.log('Hello rom Middlewarfe');
--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const { getAllTours, createTour, getTour, updateTour, deleteTour, checkID, checkBody } = require('../controllers/tourControllers')
 
+
 const router = express.Router()
-
 router.param('id', checkID)
-
 //change two middleware functions in post
 router.route('/').get(getAllTours).post(checkBody, createTour)
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
