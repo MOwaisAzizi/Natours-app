76ac57c0e0f50f80eccf0392ec00fd9340284597
owias azizi <owaisazizi360@gmail.com>
2025-01-16 12:01:50 +0430

    update some

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const dotenv = require('dotenv')
+
 dotenv.config({path:'./config.env'})
 const app = require('./app')
+const { Mongoose } = require('mongoose')
 
-//reading environment varibles
-console.log(process.env.NODE_ENV);
+//reading environment varibles,//node uses many envirnment
+// console.log(process.env.NODE_ENV);
 
 //express environment
 // console.log(app.get('env'));
-//node uses many envirnment
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD)
+Mongoose.concat(DB,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:true,
+}).then(()=>console.log('successfully connected'))
+
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
