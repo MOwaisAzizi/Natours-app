953fa72d851debb901cff5678a8a9d2966cb3b87
owias azizi <owaisazizi360@gmail.com>
2025-01-17 23:33:34 +0430

    add comments

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { getAllUsers, createUser, getUser, updateUser, deleteUser } = require('../controllers/userControllers')
 
+//using express class
 const router = express.Router()
 router.route('').get(getAllUsers).post(createUser)
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
