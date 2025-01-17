ad1c4de2f1abf06be8beb305ab5c98147a70070f
owias azizi <owaisazizi360@gmail.com>
2025-01-17 23:27:25 +0430

    some updates

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const { getAllTours, createTour, getTour, updateTour, deleteTour, checkID, checkBody } = require('../controllers/tourControllers')
 
-
 const router = express.Router()
 router.param('id', checkID)
 //change two middleware functions in post(check body)
