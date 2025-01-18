03aba69a0e06d84daedf4dcb9aaec5a453f47a8d
owias azizi <owaisazizi360@gmail.com>
2025-01-18 11:51:51 +0430

    more refectoring

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,23 +1,8 @@
-const fs = require('fs')
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
-
-exports.checkID = ((req, res, next, val) => {
-    console.log('params middleware id : ' + val);
-    if (req.params.id > tours.length * 1) {
-        return res.status(404).json({
-            status: 'Failed',
-            message: 'Not Found',
-        })
-    }
-    next()
-})
+const Tour = require('../models/tourModel')
 
 //validation of data middlware
 exports.checkBody = ((req, res, next) => {
-    const {name, price} = req.body
-    console.log(name, price);
-    
+    const {name, price} = req.body    
     if (!name || !price) {
         return res.status(404).json({
             status: 'Failed',
@@ -32,34 +17,34 @@ exports.getAllTours = (req, res) => {
         status: 'success',
         result: tours.lenght,
         time: req.requestTime,
-        data: {
-            tours
-        }
+        // data: {
+        //     tours
+        // }
     })
 }
 
 exports.createTour = (req, res) => {
-    const newId = tours[tours.length - 1].id + 1
-    const newTour = Object.assign({ id: newId }, req.body)
-    tours.push(newTour)
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tours: newTour
-            }
-        })
-    })
+    // const newId = tours[tours.length - 1].id + 1
+    // const newTour = Object.assign({ id: newId }, req.body)
+    // tours.push(newTour)
+    // fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    //     res.status(201).json({
+    //         status: 'success',
+    //         data: {
+    //             tours: newTour
+    //         }
+    //     })
+    // })
 }
 
 exports.getTour = (req, res) => {
     const id = req.params.id * 1
-    const tour = tours.find(el => el.id === id)
+    // const tour = tours.find(el => el.id === id)
     res.status(200).json({
         status: 'success',
-        data: {
-            tour
-        }
+        // data: {
+        //     tour
+        // }
     })
 }
 
--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const { getAllTours, createTour, getTour, updateTour, deleteTour, checkID, checkBody } = require('../controllers/tourControllers')
+const { getAllTours, createTour, getTour, updateTour, deleteTour, checkBody } = require('../controllers/tourControllers')
 
 const router = express.Router()
-router.param('id', checkID)
+// router.param('id', checkID)
 //change two middleware functions in post(check body)
 router.route('/').get(getAllTours).post(checkBody, createTour)
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
