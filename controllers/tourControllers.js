a15bf83b435de56b6671ab7fa8719b40e8fac9b8
owias azizi <owaisazizi360@gmail.com>
2025-01-18 12:17:44 +0430

    some more updat

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -23,18 +23,23 @@ exports.getAllTours = (req, res) => {
     })
 }
 
-exports.createTour = (req, res) => {
-    // const newId = tours[tours.length - 1].id + 1
-    // const newTour = Object.assign({ id: newId }, req.body)
-    // tours.push(newTour)
-    // fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    //     res.status(201).json({
-    //         status: 'success',
-    //         data: {
-    //             tours: newTour
-    //         }
-    //     })
-    // })
+exports.createTour = async (req, res) => {
+try{
+        // const Tour = new Tour({})
+    // Tour.sava().then
+   const newTour = await Tour.create(req.body)
+   res.status(200).json({
+    status:'success',
+    data : {
+        newTour
+    }
+   })
+}catch(err){
+    res.status(400).json({
+        status:'failed',
+        message:'invalid Data!'
+    })
+}
 }
 
 exports.getTour = (req, res) => {
