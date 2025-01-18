d86b86f0fe8cad797abcfa034aba840a3428239b
owias azizi <owaisazizi360@gmail.com>
2025-01-18 11:44:25 +0430

    refactoring to MVC

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const morgan = require('morgan')
 
 const app = express()
+const morgan = require('morgan')
 const userRouter = require('./routes/userRouter')
 const tourRouter = require('./routes/tourRouter')
+
 //this is for just shoing the morgan(to show some states of requst like request or success...) whin the app is runing
 if(process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
--- server.js
+++ server.js
@@ -4,12 +4,6 @@ dotenv.config({path:'./config.env'})
 const mongoose = require('mongoose')
 const app = require('./app')
 
-//reading environment varibles,//node uses many envirnment
-// console.log(process.env.NODE_ENV);
-
-//express environment
-// console.log(app.get('env'));
-
 const DB = process.env.DAtABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 mongoose.connect(DB,{
     useNewUrlParser:true,
@@ -19,36 +13,25 @@ mongoose.connect(DB,{
     console.log('errr ❌😂',err);
 })
 
-const tourSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        require: [true,'A tour must have a name'],
-        unique:true
-    },
-    rating:{
-        type: Number,
-        default: 4.5
-    },
-    price:{
-        type:Number,
-        require:[true,'A tour must have a price'],
-    }
-})
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-    const testTour = new Tour({
-    name:'Forest in the mountin',
-    price:500
-})
-
-testTour.save().then(doc=>{
-    console.log(doc);
-}).catch(err=>{
-    console.log('ERROR😂',err);
-})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`App listening port ${port}`);
 })
+
+//     const testTour = new Tour({
+//     name:'Forest in the mountin',
+//     price:500
+// })
+
+// testTour.save().then(doc=>{
+//     console.log(doc);
+// }).catch(err=>{
+//     console.log('ERROR😂',err);
+// })
+
+//reading environment varibles,//node uses many envirnment
+// console.log(process.env.NODE_ENV);
+
+//express environment
+// console.log(app.get('env'));
