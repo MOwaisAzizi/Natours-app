3254c5561eab2f8b6098c6815b3733b788e25ef7
owias azizi <owaisazizi360@gmail.com>
2025-01-19 10:39:54 +0430

    increase the models

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,16 +4,53 @@ const tourSchema = new mongoose.Schema({
     name:{
         type:String,
         require: [true,'A tour must have a name'],
-        unique:true
+        unique:true,
+        trim:true
     },
-    rating:{
+    duration:{
+        type:Number,
+        require: [true,'A tour must have a duration'],
+    },
+    dificulty:{
+        type:String,
+        require: [true,'A tour must have a diffeculty'],
+    },
+    maxGroupSize:{
+        type:Number,
+        require: [true,'A tour must have a groupSize'],
+    },
+    ratingAverage : {
+        type: Number,
+        default: 0
+    },
+    ratingQuantity:{
         type: Number,
         default: 4.5
     },
     price:{
         type:Number,
         require:[true,'A tour must have a price'],
-    }
+    },
+    priceDiscount:Number,
+    sammary:{
+        type:String,
+        trim:true,
+        require:[true,'A tour must have a discription'],
+    },
+    discription:{
+        type:String,
+       trim:true
+    },
+     imageCover:{
+        type:String,
+        require:[true,'A tour must have a cover image'],
+     },
+     images:[String],
+     createdAt:{
+        type : Date,
+        default:Date.now()
+    },
+     startDatas:[Date]
 })
 
 const Tour = mongoose.model('Tour', tourSchema);
