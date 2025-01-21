2605ace0ec119b093fb5dd9c84af4b708a0af0c5
owias azizi <owaisazizi360@gmail.com>
2025-01-21 11:19:29 +0430

    put it in new file

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel')
+const APIFeatures = require('./../utiles/apiFeatures')
 
 exports.aliesTopTours = (req, res, next) => {
     req.query.limit = '5'
@@ -7,55 +8,9 @@ exports.aliesTopTours = (req, res, next) => {
     next()
 }
 
-class APIFeatures {
-    constructor(query,queryObject){
-        this.query = query
-        this.queryObject = queryObject
-    }
-
-    //we return this from it becouse of chaining of object(without return it is not return any thing)
-    filter(){
-        const queryObj = {...this.queryObject}
-        const excludedField = ['sort', 'page', 'fields', 'limit']
-        excludedField.forEach(el => delete queryObj[el])
-         let queryStr = JSON.stringify(queryObj)
-        //to add a doller sighn to our query in order to use it in mongoose
-        queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, match => `$${match}`)
-        this.query = this.query.find(JSON.parse(queryStr))
-        return this
-    }
-
-    sort(){
-        if (this.queryObject.sort) {
-            const sortBy = this.queryObject.sort.split(',').join(' ')
-            this.query = this.query.sort(sortBy)
-        } else {
-            this.query = this.query.sort('-createdAt')
-        }
-        return this
-    }
-
-    limitFields(){
-        if (this.queryObject.fields) {
-            const fields =this.queryObject.fields.split(',').join(' ')
-            this.query = this.query.select(fields)
-        }
-        return this
-    }
-
-    paginate(){
-        const page = (this.queryObject.page * 1) || 1;
-        const limit = (this.queryObject.limit * 1) || 100;
-        const skip = (page - 1) * limit;
-        this.query = this.query.skip(skip).limit(limit);
-       return this
-    }
-}
-
 exports.getAllTours = async (req, res) => {
     try {
         const Feature = new APIFeatures(Tour.find(),req.query).filter().sort().limitFields().paginate()
-        console.log(Feature);
         const tours = await Feature.query
 
         //SEND RESPOSE
---
+++ utiles/apiFeatures.js
@@ -0,0 +1,45 @@
+class APIFeatures {
+    constructor(query,queryObject){
+        this.query = query
+        this.queryObject = queryObject
+    }
+
+    //we return this from it becouse of chaining of object(without return it is not return any thing)
+    filter(){
+        const queryObj = {...this.queryObject}
+        const excludedField = ['sort', 'page', 'fields', 'limit']
+        excludedField.forEach(el => delete queryObj[el])
+         let queryStr = JSON.stringify(queryObj)
+        //to add a doller sighn to our query in order to use it in mongoose
+        queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, match => `$${match}`)
+        this.query = this.query.find(JSON.parse(queryStr))
+        return this
+    }
+
+    sort(){
+        if (this.queryObject.sort) {
+            const sortBy = this.queryObject.sort.split(',').join(' ')
+            this.query = this.query.sort(sortBy)
+        } else {
+            this.query = this.query.sort('-createdAt')
+        }
+        return this
+    }
+
+    limitFields(){
+        if (this.queryObject.fields) {
+            const fields =this.queryObject.fields.split(',').join(' ')
+            this.query = this.query.select(fields)
+        }
+        return this
+    }
+
+    paginate(){
+        const page = (this.queryObject.page * 1) || 1;
+        const limit = (this.queryObject.limit * 1) || 100;
+        const skip = (page - 1) * limit;
+        this.query = this.query.skip(skip).limit(limit);
+       return this
+    }
+}
+module.exports = APIFeatures
