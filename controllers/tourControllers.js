b63013411efa455f6f0dce8eeeb9a1450cf54e6d
owias azizi <owaisazizi360@gmail.com>
2025-01-26 18:25:05 +0430

    some update

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -39,12 +39,12 @@ exports.getTour = catchAsync (async (req, res,next) => {
 
 
 exports.createTour =  catchAsync (async(req, res, next) => {
-        const tour = await Tour.create(req.body)
+        const newTour = await Tour.create(req.body)
         
         res.status(201).json({
             status: 'success',
             data: {
-                tour
+                tour:newTour
             }
         })
 })
