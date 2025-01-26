af89f12add1dcafc238a3782a07707527056f7a9
owias azizi <owaisazizi360@gmail.com>
2025-01-26 14:24:30 +0430

    update

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,8 +1,89 @@
 const mongoose = require('mongoose')
 const { default: slugify } = require('slugify')
 
+// const tourSchema = new mongoose.Schema(
+//     //object difinition
+//     {
+//         name: {
+//             type: String,
+//             // required: [true, 'A tour must have a name'],
+//             unique: true,
+//             trim: true,
+//             maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+//             minlength: [10, 'A tour name must have more or equal then 10 characters']
+//             //validators(requirt is also include)
+//         },
+//         slug: String,
+//         duration: {
+//             type: Number,
+//             // required: [true, 'A tour must have a duration'],
+//         },
+//         difficulty: {
+//             type: String,
+//             // required: [true, 'A tour must have a difficulty'],
+//             //validators
+//             enum: {
+//                 values: ['difficult', 'meduim', 'easy'],
+//                 message: 'Difficulty must be either: difficulty, meduim, easy'
+//             }
+//         },
+//         maxGroupSize: {
+//             type: Number,
+//             // required: [true, 'A tour must have a groupSize'],
+//         },
+//         ratingsAverage: {
+//             type: Number,
+//             default: 4.5,
+//             min: [1, 'Rating must be above 1.0'],
+//             max: [5, 'Rating must be below 5.0']
+//         },
+//         ratingsQuantity: {
+//             type: Number,
+//             default: 4.5
+//         },
+//         price: {
+//             type: Number,
+//             // required: [true, 'A tour must have a price'],
+//         },
+//         priceDiscount: {
+//             type: Number,
+//             //custom validat
+//             validate: {
+//                 validator: function (val) {
+//                     //this points to current document on new creation document not update
+//                     return val < this.price
+//                 },
+//                 message: 'Discount ({VALUE}) must be less then price'
+//             }
+//         },
+//         sammary: {
+//             type: String,
+//             trim: true,
+//             // required: [true, 'A tour must have a discription'],
+//         },
+//         discription: {
+//             type: String,
+//             trim: true
+//         },
+//         imageCover: {
+//             type: String,
+//             // required: [true, 'A tour must have a cover image'],
+//         },
+//         images: [String],
+//         createdAt: {
+//             type: Date,
+//             default: Date.now(),
+//             select: false
+//         },
+//         summary: String,
+//         startDates: [Date],
+//         secretTour: {
+//             type: Boolean,
+//             default: false
+//         }
+//     },
+
 const tourSchema = new mongoose.Schema(
-    //object difinition
     {
         name: {
             type: String,
@@ -11,25 +92,23 @@ const tourSchema = new mongoose.Schema(
             trim: true,
             maxlength: [40, 'A tour name must have less or equal then 40 characters'],
             minlength: [10, 'A tour name must have more or equal then 10 characters']
-            //validators(requirt is also include)
         },
         slug: String,
         duration: {
             type: Number,
-            require: [true, 'A tour must have a duration'],
+            required: [true, 'A tour must have a duration']
         },
         difficulty: {
             type: String,
-            require: [true, 'A tour must have a difficulty'],
-            //validators
+            required: [true, 'A tour must have a difficulty'],
             enum: {
-                values: ['difficult', 'meduim', 'easy'],
-                message: 'Difficulty must be either: difficulty, meduim, easy'
+                values: ['difficult', 'medium', 'easy'],
+                message: 'Difficulty must be either: difficult, medium, easy'
             }
         },
         maxGroupSize: {
             type: Number,
-            require: [true, 'A tour must have a groupSize'],
+            required: [true, 'A tour must have a group size']
         },
         ratingsAverage: {
             type: Number,
@@ -39,35 +118,33 @@ const tourSchema = new mongoose.Schema(
         },
         ratingsQuantity: {
             type: Number,
-            default: 4.5
+            default: 0
         },
         price: {
             type: Number,
-            require: [true, 'A tour must have a price'],
+            required: [true, 'A tour must have a price']
         },
         priceDiscount: {
             type: Number,
-            //custom validat
             validate: {
                 validator: function (val) {
-                    //this points to current document on new creation document not update
-                    return val < this.price
+                    return val < this.price;
                 },
-                message: 'Discount ({VALUE}) must be less then price'
+                message: 'Discount price ({VALUE}) should be below the regular price'
             }
         },
-        sammary: {
+        summary: {
             type: String,
             trim: true,
-            require: [true, 'A tour must have a discription'],
+            required: [true, 'A tour must have a summary']
         },
-        discription: {
+        description: {
             type: String,
             trim: true
         },
         imageCover: {
             type: String,
-            require: [true, 'A tour must have a cover image'],
+            required: [true, 'A tour must have a cover image']
         },
         images: [String],
         createdAt: {
@@ -75,13 +152,18 @@ const tourSchema = new mongoose.Schema(
             default: Date.now(),
             select: false
         },
-        summary: String,
         startDates: [Date],
         secretTour: {
             type: Boolean,
             default: false
         }
     },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
+    }
+);
+
     //objects for options
     {
         toJSON: { virtuals: true },
@@ -94,14 +176,14 @@ tourSchema.virtual('durationsWeek').get(function () {
 })
 
 //DOCUMENT middeware: run before or after sava() and create() methods
-// tourSchema.pre('save', function (next) {
-//     //this, points to current working document
-//     console.log('saving=====');
+tourSchema.pre('save', function (next) {
+    //this, points to current working document
+    console.log('saving===😍🤑🤑😍😍😍==');
     
-//     console.log(this.name);
-//     // this.slug = slugify(this.name,{lower:true})
-//     next()
-// })
+    console.log(this);
+    // this.slug = slugify(this.name,{lower:true})
+    next()
+})
 
 // tourSchema.post('save',function(doc,next){
 //    console.log('this will run after save');
