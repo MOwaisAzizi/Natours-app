5f06acfb9d816ba7a5e87bfcc4cb2df1696baea3
owias azizi <owaisazizi360@gmail.com>
2025-01-26 11:47:52 +0430

    debugin

--- utiles/appError.js
+++ utiles/appError.js
@@ -6,7 +6,7 @@
       this.statusCode = statusCode
       this.status = `${statusCode}`.startsWith(4) ? 'fail' : 'error'
       this.isOperational = true
-    //   Error.captureStackTrace(this.this.constructor)
+      Error.captureStackTrace(this,this.constructor)
     }
   }
 
