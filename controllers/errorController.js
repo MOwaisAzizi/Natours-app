530a062402da09a125f83274384f47817901700a
owias azizi <owaisazizi360@gmail.com>
2025-01-28 22:58:33 +0430

    delete comment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,6 @@
 const AppError = require("./../utiles/appError");
 
 
-//error handlin in get update and create......
 const handleObjectIdDB = (err) => {
    const message = `invalid ${err.path}: ${err.value}`;
    return new AppError(message, 400);
