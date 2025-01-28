77500019c96cd2715a95ec72ce2570909d188c11
owias azizi <owaisazizi360@gmail.com>
2025-01-28 16:21:16 +0430

    some update

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,6 +1,6 @@
 const AppError = require("./../utiles/appError");
 
-const handleCastErrorDB = (err) => {
+const handleObjectIdDB = (err) => {
    const message = `invalid ${err.path}: ${err.value}`;
    return new AppError(message, 400);
 };
@@ -11,33 +11,37 @@ const handleDuplicateFieldsDB = (err) => {
    return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = (err) => {
+   
+   const errors = Object.values(err.errors).map(el=>el.message)
+   const message = `Indvalid data. ${errors.join(' .')}`;
+   return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
-   if (err.isOperational) {
       res.status(err.statusCode).json({
          status: err.status,
          message: err.message,
          stack: err.stack,
          error: err,
-      });
-   } else {
-      console.log('Error🔥');
-      res.status(err.statusCode).json({
-         status: 'error',
-         message: 'Something went wrong very well!',
-      });
-   }
+   })
 };
 
 const sendErrorProd = (err, res) => {
+   if (err.isOperational) {
    res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
    });
+} else {
+   res.status(err.statusCode).json({
+      status: err.status,
+      message: 'someting went wrong',
+   });
+}
 };
 
 module.exports = ((err, req, res, next) => {
-   console.log(err.stack);
-
    err.statusCode = err.statusCode || 500;
    err.status = err.status || 'error';
 
@@ -45,11 +49,11 @@ module.exports = ((err, req, res, next) => {
       sendErrorDev(err, res);
    } else if (process.env.NODE_ENV === 'production') {
       let error = { ...err };
-      console.log('production environment');
-
-      if (error.kind === 'ObjectId') error = handleCastErrorDB(error);
+      
+      if (error.kind === 'ObjectId') error = handleObjectIdDB(error);
       if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-
+      if (error._message === 'Validation failed') error = handleValidationErrorDB(error);
+      
       sendErrorProd(error, res);
    }
 });
