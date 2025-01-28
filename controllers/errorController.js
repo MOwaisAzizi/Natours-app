381468ba0110cc5a9be7b47dac0665e5da59231b
owias azizi <owaisazizi360@gmail.com>
2025-01-28 15:14:17 +0430

    error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,69 +1,55 @@
-const AppError = require("../utiles/appError");
+const AppError = require("./../utiles/appError");
 
-const handleCastErrorDB = (err)=>{
-   const message = `invalid ${err.path}: ${err.value}`
-   return new AppError(message,400)
-}
+const handleCastErrorDB = (err) => {
+   const message = `invalid ${err.path}: ${err.value}`;
+   return new AppError(message, 400);
+};
 
-const hanldeDuplicateFieldsDB = (err)=>{
-   // const value = err.mserr.match(/(["'])(\\?.)*?\1/)
-   const value = err.keyValue.name
-   console.log(value);
-   const message = `Duplicate feild : ${value}. please use another value`
-   return new AppError(message,400)
-   
-}
+const handleDuplicateFieldsDB = (err) => {
+   const value = err.keyValue.name;
+   const message = `Duplicate field: ${value}. Please use another value.`;
+   return new AppError(message, 400);
+};
 
-const sendErrorDev  = (err,res)=>{
-   //errors from operations like tacking wrong routs id or so on
-  if(err.isOperational){
-   res.status(err.statusCode).json({
-      status:err.status,
-      message:err.message,
-      stack:err.stack,
-      error:err
-   })
-  }else{
- //programing Error or unknown error : generic controll
-   console.log('Error🔥');
-   res.status(err.statusCode).json({
-      status:'Error',
-      message:'Someting went wrong very well!'
-   })
-  }
-}
-
-const sendErrorProd = (err,res)=>{
-   res.status(err.statusCode).json({
-      status:err.status,
-      message:err,
-   })
-}
-
-
-module.exports = ((err,req,res,next)=>{
-    //for tracing whare error happend
-    console.log(err.stack);
-    
-    err.statusCode = err.statusCode || 500
-   err.status = err.status || 'error'
-
-   if(process.env.NODE_ENV === 'developement'){
-      sendErrorDev(err,res)
+const sendErrorDev = (err, res) => {
+   if (err.isOperational) {
+      res.status(err.statusCode).json({
+         status: err.status,
+         message: err.message,
+         stack: err.stack,
+         error: err,
+      });
+   } else {
+      console.log('Error🔥');
+      res.status(err.statusCode).json({
+         status: 'error',
+         message: 'Something went wrong very well!',
+      });
    }
-      else if(process.env.NODE_ENV === 'production'){
-       let error = {...err}
-       console.log('production environment');
-      //  console.log(error);
-       
-       //for in inputing wrong id route for geting nice data in production
-       //reassign the error to sendError(we want to short the message becouse it has so messay key value)
-         if(error.name==='CastError') error = handleCastErrorDB(error)
-         if(error.code===11000) error = hanldeDuplicateFieldsDB(error)
-       console.log(error);
-       
-       sendErrorProd(error,res)
-      }
-})
+};
 
+const sendErrorProd = (err, res) => {
+   res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+   });
+};
 
+module.exports = ((err, req, res, next) => {
+   console.log(err.stack);
+
+   err.statusCode = err.statusCode || 500;
+   err.status = err.status || 'error';
+
+   if (process.env.NODE_ENV === 'development') {
+      sendErrorDev(err, res);
+   } else if (process.env.NODE_ENV === 'production') {
+      let error = { ...err };
+      console.log('production environment');
+
+      if (error.kind === 'ObjectId') error = handleCastErrorDB(error);
+      if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+
+      sendErrorProd(error, res);
+   }
+});
