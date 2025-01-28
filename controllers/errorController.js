7abdc99c6a12d0cd908c8c8bcc7d7ba78b3ef000
owias azizi <owaisazizi360@gmail.com>
2025-01-28 10:21:53 +0430

    more more advenced error handling in prod and dev

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,21 @@
 const sendErrorDev  = (err,res)=>{
+
+   //errors from operations like tacking wrong routs id or so on
+  if(err.isOperational){
    res.status(err.statusCode).json({
       status:err.status,
       message:err.message,
       stack:err.stack,
       error:err
    })
+  }else{
+ //programing Error or unknown error : generic controll
+   console.log('Error🔥');
+   res.status(err.statusCode).json({
+      status:'Error',
+      message:'Someting went wrong very well!'
+   })
+  }
 }
 
 const sendErrorProd = (err,res)=>{
