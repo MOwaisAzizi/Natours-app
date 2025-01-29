aad26f32d2a2ecf3fd8eb72aa3070ccd08472eaf
owias azizi <owaisazizi360@gmail.com>
2025-01-29 14:52:59 +0430

    creation of user and routes for that

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/userModel')
 const catchAsycn = require('../utiles/catchAsync')
 
-module.signup = catchAsycn (async(req,res,next)=>{
+exports.signup = catchAsycn (async(req,res,next)=>{
      const newUser = await User.create(req.body)
      res.status(201).json({
         status:'success',
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const { getAllUsers, createUser, getUser, updateUser, deleteUser } = require('../controllers/userControllers')
+const { signup } = require('../controllers/authController')
 
 //using express class
 const router = express.Router()
+router.route('/signup',signup)
 router.route('').get(getAllUsers).post(createUser)
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
 
