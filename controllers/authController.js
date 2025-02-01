32900b600b31e116152062db32f55c289d9e5341
owias azizi <owaisazizi360@gmail.com>
2025-02-01 19:40:06 +0430

    creation of login function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
+const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 const catchAsycn = require('../utiles/catchAsync')
+const appError = require('../utiles/appError')
+
 
 exports.signup = catchAsycn (async(req,res,next)=>{
     //  const newUser = await User.create(req.body)
@@ -9,10 +12,31 @@ exports.signup = catchAsycn (async(req,res,next)=>{
         password:'fjfjfjfj',
         passwordConfirm:'fjfjfjfj',
      })
+
+const token = jwt.sign({id:newUser._id}, process.env.JWT_SECRET,{
+    expiresIn:process.env.JWT_EXPIRES_IN
+})
      res.status(201).json({
         status:'success',
+        token,
         data:{
             user: newUser
         }
      })
 })
+
+exports.login = function(req,res,next){
+    //check if email or password exist
+ if(!email || password){
+    return next(appError('please provide email and password!') )
+ }
+
+ //check if email and password correct
+   
+ //if every thing is ok do this
+ const token = ''
+ res.status(200).json({
+    status:'success',
+    token,
+ })
+}
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const { getAllUsers, createUser, getUser, updateUser, deleteUser } = require('../controllers/userControllers')
+const { getAllUsers, createUser, getUser, updateUser, deleteUser,login } = require('../controllers/userControllers')
 const { signup } = require('../controllers/authController')
 
 //using express class
 const router = express.Router()
+
 router.post('/signup',signup)
+router.post('/login',login)
+
 router.route('').get(getAllUsers).post(createUser)
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
 
