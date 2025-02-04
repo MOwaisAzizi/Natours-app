dcc3353d0db0ef8565df32c7ffecd0b858c69635
owias azizi <owaisazizi360@gmail.com>
2025-02-04 17:41:58 +0430

    working with email sending

--- utiles/email.js
+++ utiles/email.js
@@ -1,14 +1,23 @@
-const sendEmail = options=>{
+const nodemailer = require('nodemailer')
+
+const sendEmail = async options=>{
   //create transporter
-  const transporter = nodemailer.createTransporter({
-    services:'Gmail',
+  const transporter = nodemailer.createTransport({
+    host:process.env.EMAIL_HOST,
+    port:process.env.EMAIL_PORT,
     auth:{
         user:process.env.EMAIL_USERNAME,
         pass:process.env.EMAIL_PASSWORD,
     }
-    //activate in gamil(less secure app)
   })
   //define email options
-
+  const mailOptions = {
+    from:'Owais Azizi <hello@woais.io>',
+    to:options.email,
+    subject:options.subject,
+    text:options.message,
+  }
   //send email
+  await transporter.sendEmail(mailOptions)
 }
+module.exports = sendEmail
