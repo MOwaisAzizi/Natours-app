fb040b58d6aeb1907a0f02fc587d307e9f3f597b
owias azizi <owaisazizi360@gmail.com>
2025-02-07 20:14:35 +0430

    some comments

--- controllers/tourControllers-v2.js
+++ controllers/tourControllers-v2.js
@@ -1,154 +1,154 @@
-const Tour = require('../models/tourModel')
+// const Tour = require('../models/tourModel')
 
-exports.aliesTopTours = (req, res, next) => {
-    req.query.limit = '5'
-    req.query.sort = '-ratingAverage,price'
-    req.query.fields = 'price,name,difficulty,ratingAverage,summary'
-    next()
-}
+// exports.aliesTopTours = (req, res, next) => {
+//     req.query.limit = '5'
+//     req.query.sort = '-ratingAverage,price'
+//     req.query.fields = 'price,name,difficulty,ratingAverage,summary'
+//     next()
+// }
 
-//first version
+// //first version
 
-exports.getAllTours = async (req, res) => {
-    try {
-        // const query = Tour.find().where('duration').equals(5).where('dificuly').equals('easy')
-        // const tours = Tour.find({duration:20})  //  const tours = Tour.find(req.query)
+// exports.getAllTours = async (req, res) => {
+//     try {
+//         // const query = Tour.find().where('duration').equals(5).where('dificuly').equals('easy')
+//         // const tours = Tour.find({duration:20})  //  const tours = Tour.find(req.query)
 
-        //1-BUILD QUERY
-        const queryOBJ = { ...req.query }
+//         //1-BUILD QUERY
+//         const queryOBJ = { ...req.query }
 
-        //2A-filtering
-        //we extract these queries form our filtering for better working with it
-        const excludedField = ['sort', 'page', 'fields', 'limit']
-        excludedField.forEach(el => delete queryOBJ[el])
+//         //2A-filtering
+//         //we extract these queries form our filtering for better working with it
+//         const excludedField = ['sort', 'page', 'fields', 'limit']
+//         excludedField.forEach(el => delete queryOBJ[el])
 
-        //2B-advence Filtering
-        let queryStr = JSON.stringify(queryOBJ)
-        //to add a doller sighn to our query in order to use it in mongoose
-        queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, match => `$${match}`)
-        //  console.log(JSON.parse(queryStr));
+//         //2B-advence Filtering
+//         let queryStr = JSON.stringify(queryOBJ)
+//         //to add a doller sighn to our query in order to use it in mongoose
+//         queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, match => `$${match}`)
+//         //  console.log(JSON.parse(queryStr));
 
-        //we are doing this for chaining the prototype methods of find(by using directly await it is imposible becouse it compack with document using first method)
-        let query = Tour.find(JSON.parse(queryStr))
+//         //we are doing this for chaining the prototype methods of find(by using directly await it is imposible becouse it compack with document using first method)
+//         let query = Tour.find(JSON.parse(queryStr))
 
-        //3-sorting
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ')
-            query = query.sort(sortBy)
-        } else {
-            query = query.sort('-createdAt')
-        }
+//         //3-sorting
+//         if (req.query.sort) {
+//             const sortBy = req.query.sort.split(',').join(' ')
+//             query = query.sort(sortBy)
+//         } else {
+//             query = query.sort('-createdAt')
+//         }
 
-        //4-fields
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ')
-            query = query.select(fields)
-        }
+//         //4-fields
+//         if (req.query.fields) {
+//             const fields = req.query.fields.split(',').join(' ')
+//             query = query.select(fields)
+//         }
         
-        //5-page pagination
-        const page = (req.query.page * 1) || 1;
-        const limit = (req.query.limit * 1) || 100;
-        const skip = (page - 1) * limit;
-        query = query.skip(skip).limit(limit);
+//         //5-page pagination
+//         const page = (req.query.page * 1) || 1;
+//         const limit = (req.query.limit * 1) || 100;
+//         const skip = (page - 1) * limit;
+//         query = query.skip(skip).limit(limit);
 
-        if (req.query.page) {
-            const numTours = await Tour.countDocuments();
-            if (skip >= numTours) throw new Error('This page does not exist');
-        }
+//         if (req.query.page) {
+//             const numTours = await Tour.countDocuments();
+//             if (skip >= numTours) throw new Error('This page does not exist');
+//         }
 
-        const tours = await query
+//         const tours = await query
 
-        //SEND RESPOSE
-        res.status(200).json({
-            status: 'success',
-            result: tours.lenght,
-            data: {
-                tours
-            }
-        })
-    } catch (err) {
-        res.status(404).json({
-            status: 'failed',
-            message: err
-        })
-    }
-}
-
-
-exports.getTour = async (req, res) => {
-    try {
-        const tour = await Tour.findById(req.params.id)
-        // const tour = await Tour.findOne({_id:req.parmas.id})
-        //tour.save 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                tour
-            }
-        })
-    } catch (err) {
-        res.status(404).json({
-            status: 'failed',
-            message: 'invalid Data!'
-        })
-    }
-}
-
-exports.createTour = async (req, res) => {
-    try {
-        // const Tour = new Tour({})
-        // Tour.sava()     save is the prototye object of the dindone class
-        const tour = await Tour.create(req.body)
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour
-            }
-        })
-    } catch (err) {
-        res.status(400).json({
-            status: 'failed',
-            message: 'invalid Data!'
-        })
-    }
-}
-
-exports.updateTour = async (req, res) => {
-    try {
-        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        });
-
-        res.status(200).json({
-            status: 'success',
-            data: {
-                tour
-            }
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'failed',
-            message: 'Invalid Data!'
-        });
-    }
-};
+//         //SEND RESPOSE
+//         res.status(200).json({
+//             status: 'success',
+//             result: tours.lenght,
+//             data: {
+//                 tours
+//             }
+//         })
+//     } catch (err) {
+//         res.status(404).json({
+//             status: 'failed',
+//             message: err
+//         })
+//     }
+// }
 
 
-exports.deleteTour = async (req, res) => {
-    try {
+// exports.getTour = async (req, res) => {
+//     try {
+//         const tour = await Tour.findById(req.params.id)
+//         // const tour = await Tour.findOne({_id:req.parmas.id})
+//         //tour.save 
+//         res.status(200).json({
+//             status: 'success',
+//             data: {
+//                 tour
+//             }
+//         })
+//     } catch (err) {
+//         res.status(404).json({
+//             status: 'failed',
+//             message: 'invalid Data!'
+//         })
+//     }
+// }
 
-        //commonlly we do not return someting
-        await Tour.findByIdAndDelete(req.params.id);
+// exports.createTour = async (req, res) => {
+//     try {
+//         // const Tour = new Tour({})
+//         // Tour.sava()     save is the prototye object of the dindone class
+//         const tour = await Tour.create(req.body)
+//         res.status(201).json({
+//             status: 'success',
+//             data: {
+//                 tour
+//             }
+//         })
+//     } catch (err) {
+//         res.status(400).json({
+//             status: 'failed',
+//             message: 'invalid Data!'
+//         })
+//     }
+// }
 
-        res.status(204).json({
-            status: 'success',
-            data: null
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'failed',
-            message: err
-        });
-    }
-}
+// exports.updateTour = async (req, res) => {
+//     try {
+//         const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+//             new: true,
+//             runValidators: true
+//         });
+
+//         res.status(200).json({
+//             status: 'success',
+//             data: {
+//                 tour
+//             }
+//         });
+//     } catch (err) {
+//         res.status(404).json({
+//             status: 'failed',
+//             message: 'Invalid Data!'
+//         });
+//     }
+// };
+
+
+// exports.deleteTour = async (req, res) => {
+//     try {
+
+//         //commonlly we do not return someting
+//         await Tour.findByIdAndDelete(req.params.id);
+
+//         res.status(204).json({
+//             status: 'success',
+//             data: null
+//         });
+//     } catch (err) {
+//         res.status(404).json({
+//             status: 'failed',
+//             message: err
+//         });
+//     }
+// }
