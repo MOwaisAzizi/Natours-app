8fe1fc1b1d1993e1a56821caac66c516a54379b7
owias azizi <owaisazizi360@gmail.com>
2025-02-09 17:14:51 +0430

    creation of defining child referencing to parrent

--- models/tourModel-v1.js
+++ models/tourModel-v1.js
@@ -105,17 +105,7 @@ const tourSchema = new mongoose.Schema(
           day:Number
         }
       ],
-      //for the way of embeding users in tour
-      // guides:Array
 
-      //child referencing way of referencing to users
-       guides:[
-         {
-          type:mongoose.Schema.ObjectId,
-          ref:'User'
-         }
-       ]
-       
     },
 
     {
@@ -127,19 +117,49 @@ const tourSchema = new mongoose.Schema(
   tourSchema.virtual('durationWeeks').get(function() {
     return this.duration / 7;
   });
- 
-  //for the way of embeding users in tour
-  // tourSchema.pre('save',async function(next){
-    // const guidePromise = this.guides.map(async id=> User.findById(id))
-    // this.guides = await Promise.all(guidePromise)
-  //   next()
-  // })
 
+//DOCUMENT middeware: run before or after sava() and create() methods
+tourSchema.pre('save', function (next) {
+    //this,'Point to current working document
+    // this.slug = slugify(this.name,{lower:true})
+    next()
+})
+
+//after save and create
+// tourSchema.post('save',function(doc,next){
+//    console.log('this will run after save');
+//    console.log(doc);
+//    next()
+// })
+
+
+//Query middleware:run before queries like find and this points to query object and access to query methods
+///^find:means that every query that starts with find(we do this becuse of applying to single find tour too),pre:before,post:after/
+//we can use tourSchema.pre('findOne') for single find
+// tourSchema.pre(/^find/, function(next) {
+//     //bring all in the find method in controller but process this query after finding your result
+//     this.find({ secretTour: { $ne: true } });
+//     this.start = Date.now()
+//     next();
+// });
+
+// tourSchema.post(/^find/, function() {
+//     console.log(`The operation took ${Date.now() - this.start} milliseconds`);
+// });
+
+//3-Agregate middleware: becuse when filtering the secret tour in find method the aggregate request alse filter it we use this
 tourSchema.pre('aggregate', function (next) {
+    // console.log('This is aggregate');
+    // Add a $match stage to filter out secret tours
     this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
     this.start = Date.now();
     next()
 });
+
+// tourSchema.post('aggregate', function () {
+//     console.log('This is finish');
+//     console.log(`The time took ${Date.now() - this._start} ms`);
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour
