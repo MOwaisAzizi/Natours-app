428153b35ca6bdfb8e08c49c90a6ccca818d2ecd
owias azizi <owaisazizi360@gmail.com>
2025-02-11 15:09:37 +0700

    creation of express nested route

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -2,8 +2,11 @@ const express = require('express')
 const  {restrictTo,protect} = require('../controllers/authController')
 const {getAllReview,createReview} = require('../controllers/reviewController')
 
-const router = express.Router()
+//merge params true means that the crreateRevewu can use the params comming from tourRouter 
+const router = express.Router({mergeParams: true})
 
+//Post: tour/:kdfkde3/reviews
+//get: tour/:kdfkde3/reviews
 router.route('/').get(getAllReview).post(protect,restrictTo('user'),createReview)
 
 module.exports = router
--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -1,21 +1,27 @@
 const express = require('express')
 const { getAllTours, createTour, getTour, updateTour, deleteTour,aliesTopTours,getTourStats,getMonthlyPlan } = require('../controllers/tourControllers')
 const {protect,restrictTo} = require('../controllers/authController')
-const {createReview} = require('../controllers/reviewController')
+// const {createReview} = require('../controllers/reviewController')
+const reviewRouter = require('../routes/reviewRouter')
+const { createReview } = require('../controllers/reviewController')
 
 const router = express.Router()
 // router.param('id', checkID)
 //change two middleware functions in post(check body)
 // router.route('/').get(getAllTours).post(checkBody, createTour)
 //we can use the catchAsycn funtions here in order to send errors in global error handler
+
+//nested routes:for geting userid and tour id automaticall instead of writing it in body
+//the problem with this way is that we have reveiw rout in tour router(not good)
+// router.route('/:tourId/reviews').post(protect,restrictTo('user'),createReview)
+
+router.use('/:tourId/reviews',reviewRouter)
+
 router.route('/top-5-cheap').get(aliesTopTours,getAllTours)
 router.route('/tour-stats').get(getTourStats)
 router.route('/monthly-plan/:year').get(getMonthlyPlan)
 
 router.route('/').get(protect,getAllTours).post(createTour)
 router.route('/:id').get(getTour).patch(updateTour).delete(protect,restrictTo('admin','lead-guide'),deleteTour)
-
-//nested routes:for geting userid and tour id automaticall instead of writing it in body
-router.route('/:tourId/reviews').post(protect,restrictTo('user'),createReview)
 
 module.exports = router
