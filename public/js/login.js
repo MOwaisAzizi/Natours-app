849ccd2459d3d3d00a0cc54b2d6893df1ed0cd87
owias azizi <owaisazizi360@gmail.com>
2025-02-23 18:42:08 +0430

    debuging

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,44 @@
+/* eslint-disable */
+import axios from "axios";
 
-const login = (email,passowrd) =>{
-   alert(eamil, passowrd)
+window.onload =  function() {
+    console.log(typeof axios);
+    console.log('performing.........');
+    
 }
+
+const login = async (email,password) =>{
+    console.log('Running......');
+    
+ try{
+    const res = await axios({
+        method:'POST',
+        url:'http://127.0.0.1:3000/api/v1/user/login',
+        data: {
+            email:email,
+            password: password
+        }
+      })
+      console.log(res.data);
+      
+ }catch(err){
+    console.log(err);
+ }
+}
+
+// document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('.form')
+    console.log(form);
+    
+    if(!form) console.log('not found form');
+    
+    form.addEventListener('submit', function(e) {
+        console.log('submitting1');
+        e.preventDefault();
+        console.log('submitting2');
+        
+        // const password = document.getElementById('password').value;
+        // const email = document.getElementById('email').value;
+        // login(email, password);
+    });
+// });
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -3,14 +3,14 @@
 const data = JSON.parse(document.getElementById('map').dataset.locations)
 console.log(data);
 
-document.addEventListener("DOMContentLoaded", function () {
-    var map = L.map('map').setView([51.505, -0.09], 13);
+// document.addEventListener("DOMContentLoaded", function () {
+//     var map = L.map('map').setView([51.505, -0.09], 13);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+//     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//     }).addTo(map);
 
-    L.marker([51.505, -0.09]).addTo(map)
-        .bindPopup('نقشه ساده Leaflet')
-        .openPopup();
-});
+//     L.marker([51.505, -0.09]).addTo(map)
+//         .bindPopup('نقشه ساده Leaflet')
+//         .openPopup();
+// });
