3ec81f79258b538c06b77cba5c5eb4e036af9d7f
owias azizi <owaisazizi360@gmail.com>
2025-03-29 11:47:26 +0430

    update settings

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
-
 process.on('uncaughtException', err => {
     console.log('uncaughtException: server is shoting down...');
     console.log(err.name, err.message);
     process.exit(1)
 })
-
 
 dotenv.config({ path: './config.env' })
 const app = require('./app')
