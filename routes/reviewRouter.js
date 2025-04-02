810def0a5c0f10aff649de079d07cb033179e845
owias azizi <owaisazizi360@gmail.com>
2025-04-02 09:16:19 +0430

    delete a comment

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -2,7 +2,6 @@ const express = require('express')
 const { restrictTo, protect } = require('../controllers/authController')
 const { getAllReview, createReview, deleteReview, updateReview, setTourUserIds, getReview } = require('../controllers/reviewController')
 
-//merge params true means that the crreateRevewu can use the params comming from tourRouter 
 const router = express.Router({ mergeParams: true })
 
 router.use(protect)
