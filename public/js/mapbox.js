db0a10882f0013c1539489b9084d28e85f31f3e8
owias azizi <owaisazizi360@gmail.com>
2025-04-03 10:12:07 +0430

    delete comments

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -7,19 +7,14 @@ export const displayMap = locations => {
       container: 'map',
       style: 'mapbox://styles/jonasschmedtmann/cjvi9q8jd04mi1cpgmg7ev3dy',
       scrollZoom: false
-      // center: [-118.113491, 34.111745],
-      // zoom: 10,
-      // interactive: false
     });
   
     const bounds = new mapboxgl.LngLatBounds();
   
     locations.forEach(loc => {
-      // Create marker
       const el = document.createElement('div');
       el.className = 'marker';
   
-      // Add marker
       new mapboxgl.Marker({
         element: el,
         anchor: 'bottom'
@@ -27,7 +22,6 @@ export const displayMap = locations => {
         .setLngLat(loc.coordinates)
         .addTo(map);
   
-      // Add popup
       new mapboxgl.Popup({
         offset: 30
       })
@@ -35,7 +29,6 @@ export const displayMap = locations => {
         .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
         .addTo(map);
   
-      // Extend map bounds to include current location
       bounds.extend(loc.coordinates);
     });
   
